788c3773d5bcf617f6ee9afdf26ab07d2823324b
Jeff Forcier <jeff@bitprophet.org>
2011-10-30 15:47:19 -0700

    setup.py updates

--- setup.py
+++ setup.py
@@ -2,14 +2,27 @@ from setuptools import setup, find_packages
 
 setup(
     name='spec',
-    version="0.1",
+    version="0.9",
     description = 'Specification-style output for nose',
     author = 'Jeff Forcier',
     author_email = 'jeff@bitprophet.org',
+    url = 'https://github.com/bitprophet/spec',
     license = 'MIT',
     py_modules = ["spec"],
     entry_points = {
         'nose.plugins.0.10': [
             'spec = spec:Spec',
-        ]},
+        ]
+    },
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Testing'
+    ]
 )
