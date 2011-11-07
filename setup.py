bb82cc425353fa16aa9cdc4fbf06da53477d8799
Jeff Forcier <jeff@bitprophet.org>
2011-11-06 22:40:28 -0800

    Cut 0.9.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='spec',
-    version="0.9.2",
+    version="0.9.3",
     description='Specification-style output for nose',
     author='Jeff Forcier',
     author_email='jeff@bitprophet.org',
