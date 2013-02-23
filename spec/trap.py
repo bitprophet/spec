b8b26156a91d4c10e49f1b7ddbfe78ee29af8009
Donald Stufft <donald.stufft@gmail.com>
2013-02-23 00:38:53 -0500

    Use io.BytesIO instead of io.StringIO

--- spec/trap.py
+++ spec/trap.py
@@ -8,22 +8,23 @@ Based on original code from Fabric 1.x, specifically:
 
 Though modifications have been made since.
 """
+import io
 import sys
-from io import StringIO
+
 from functools import wraps
 
 
-class CarbonCopy(StringIO):
+class CarbonCopy(io.BytesIO):
     """
-    A StringIO capable of multiplexing its writes to other buffer objects.
+    A io.BytesIO capable of multiplexing its writes to other buffer objects.
     """
     def __init__(self, buffer='', cc=None):
         """
         If ``cc`` is given and is a file-like object or an iterable of same,
-        it/they will be written to whenever this StringIO instance is written
+        it/they will be written to whenever this io.BytesIO instance is written
         to.
         """
-        StringIO.__init__(self, buffer)
+        io.BytesIO.__init__(self, buffer)
         if cc is None:
             cc = []
         elif hasattr(cc, 'write'):
@@ -31,22 +32,22 @@ class CarbonCopy(StringIO):
         self.cc = cc
 
     def write(self, s):
-        StringIO.write(self, s)
+        io.BytesIO.write(self, s)
         for writer in self.cc:
             writer.write(s)
 
 
 def trap(func):
     """
-    Replaces sys.std(out|err) with ``StringIO``s during the test, restored after.
+    Replaces sys.std(out|err) with ``io.BytesIO``s during the test, restored after.
 
-    In addition, a new combined-streams output (another StringIO) will appear
-    at ``sys.stdall``. This StringIO will resemble what a user sees at a
+    In addition, a new combined-streams output (another io.BytesIO) will appear
+    at ``sys.stdall``. This io.BytesIO will resemble what a user sees at a
     terminal, i.e. both streams intermingled.
     """
     @wraps(func)
     def wrapper(*args, **kwargs):
-        sys.stdall = StringIO()
+        sys.stdall = io.BytesIO()
         my_stdout, sys.stdout = sys.stdout, CarbonCopy(cc=sys.stdall)
         my_stderr, sys.stderr = sys.stderr, CarbonCopy(cc=sys.stdall)
         try:
