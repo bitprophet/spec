578f123ef0a6555f92ef189800b80aadc873c8aa
Jeff Forcier <jeff@bitprophet.org>
2013-03-15 17:02:59 -0700

    Bump version to note bunch-o-changes re: Py3 support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='spec',
-    version="0.9.7",
+    version="0.10.0",
     description='Specification-style output for nose',
     author='Jeff Forcier',
     author_email='jeff@bitprophet.org',
