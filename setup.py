689052512c689f9200e394e5a88127d53244e8b0
Jeff Forcier <jeff@bitprophet.org>
2014-12-10 12:08:13 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='spec',
-    version="0.11.1",
+    version="0.11.2",
     description='Specification-style output for nose',
     author='Jeff Forcier',
     author_email='jeff@bitprophet.org',
