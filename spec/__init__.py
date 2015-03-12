61d2b5ba13039b87bb97b5f74371bf92475e4912
Jeff Forcier <jeff@bitprophet.org>
2015-03-12 16:49:25 -0700

    Port more-verbose eq_ from Fabric test suite

--- spec/__init__.py
+++ spec/__init__.py
@@ -1,6 +1,7 @@
 import six
 
 from nose import SkipTest
+# Gets us eq_, ok_, etc
 from nose.tools import *
 
 from spec.plugin import SpecPlugin
@@ -18,3 +19,31 @@ class Spec(six.with_metaclass(InnerClassParser, object)):
 # Simple helper
 def skip():
     raise SkipTest
+
+
+# Multiline string comparison helper ripped from Fabric 1.x
+def eq_(result, expected, msg=None):
+    """
+    Shadow of the Nose builtin which presents easier to read multiline output.
+    """
+    params = {'expected': expected, 'result': result}
+    aka = """
+
+--------------------------------- aka -----------------------------------------
+
+Expected:
+%(expected)r
+
+Got:
+%(result)r
+""" % params
+    default_msg = """
+Expected:
+%(expected)s
+
+Got:
+%(result)s
+""" % params
+    if (repr(result) != str(result)) or (repr(expected) != str(expected)):
+        default_msg += aka
+    assert result == expected, msg or default_msg
